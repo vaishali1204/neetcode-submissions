12c66167a9504460cfd6624c4f564667ef03232c
vaishali1204 <87437837+vaishali1204@users.noreply.github.com>
2026-09-01 13:06:47 +0530

    Add: sql-top-travellers - submission-0

---
+++ SQL/sql-top-travellers/submission-0.sql
@@ -0,0 +1,8 @@
+SELECT 
+    users.name,
+    COALESCE(SUM(rides.distance), 0) AS travelled_distance
+FROM users
+LEFT JOIN rides
+ON users.id = rides.user_id
+GROUP BY users.id, users.name
+ORDER BY travelled_distance DESC, users.name ASC;
